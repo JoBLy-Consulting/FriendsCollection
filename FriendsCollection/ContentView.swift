f0899528d764d154fb7c61bc0e7057d6b39ddf1b
Cityhunter <johan.guenaoui@gmail.com>
2020-10-01 15:18:17 +0200

    Initial Commit

---
+++ FriendsCollection/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FriendsCollection
+//
+//  Created by Johan Guenaoui on 01/10/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FriendsCollection/FriendsCollectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsCollectionApp.swift
+//  FriendsCollection
+//
+//  Created by Johan Guenaoui on 01/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct FriendsCollectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
